fc730666dbb55db26d2044bcbdd0d87b5517193a
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-10-22 10:44:53 +0100

    Create dogs.js

---
+++ dogs.js
@@ -0,0 +1,12 @@
+const Dogs = [
+  {name: 'Rover', breed: 'Corgi', likesCats: false, age: 7},
+  {name: 'Floss', breed: 'Border Collie', likesCats: true, age: 1},
+  {name: 'Jeff', breed: 'Tibetan Mastiff', likesCats: false, age: 10},
+  {name: 'Mr Woofton IV', breed: 'Pug', likesCats: false, age: 14},
+  {name: 'Skippy', breed: 'Shiba Inu', likesCats: false, age: 2}
+]
+
+//Pupppies filter under age of 3
+//Sort by age
+//Reduce to only those who like likesCats
+//Map - change it to just the names of the dogs
